97a1e4faff3777b470a4a296633189011e54ded8
keith72632 <keithhorton30@outlook.com>
2021-02-05 19:54:51 -0600

    added play

--- Youtube/play.s
+++ Youtube/play.s
@@ -6,8 +6,30 @@ eight: .word 0
 
 .global _start
 _start:
-	mov r7, #3:
+	ldr r1, _five
+	mov r3, #10
+	str r3, [r1]	
+	ldr r2, _eight
+	mov r3, #5
+	str r3, [r2]
+
+	ldr r1, _five
+	ldr r1, [r1]
+	ldr r2, _eight
+	ldr r2, [r2]
+
+	add r0, r1, r2
+	mov r4, #10
+	cmp r0, r4
+	blt end
+	beq  _less
+	mov r0, #6
+_less:
+	mov r0, #22
 
 end:
 	mov r7, #1
 	swi 0
+
+_five: .word five
+_eight: .word eight
